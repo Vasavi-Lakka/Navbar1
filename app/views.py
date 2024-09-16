020ecdbe3d7e793495cdd8ee568d1b7701300bea
Your Name <vasavilakka398@gmail.com>
2024-09-16 22:41:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render # type: ignore
+
+# Create your views here.
+def temp1(request):
+    return render(request, 'temp1.html')
+def temp2(request):
+    return render(request, 'temp2.html')
+def temp3(request):
+    return render(request, 'temp3.html')
+def temp4(request):
+    return render(request, 'temp4.html')
+def temp5(request):
+    return render(request, 'temp5.html')
+def temp6(request):
+    return render(request, 'temp6.html')
